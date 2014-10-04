88c0cce947ab9e9ecd0caea1799eb503c68abb3b
Edward Huynh <edward@edwardhuynh.com>
2014-10-04 11:46:34 +1000

    initial commit

---
+++ ShowcaseKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShowcaseKit
+//
+//  Created by Edward Huynh on 4/10/2014.
+//  Copyright (c) 2014 Edward Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShowcaseKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShowcaseKit
+//
+//  Created by Edward Huynh on 4/10/2014.
+//  Copyright (c) 2014 Edward Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
