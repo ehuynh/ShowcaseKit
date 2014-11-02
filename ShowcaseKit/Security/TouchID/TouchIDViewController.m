d39a99a94bc6b7bace61f7e442ae158706a620f7
Edward Huynh <edward@edwardhuynh.com>
2014-11-02 17:24:57 +1100

    added code to perform touch id and handle events

--- ShowcaseKit/Security/TouchID/TouchIDViewController.m
+++ ShowcaseKit/Security/TouchID/TouchIDViewController.m
@@ -8,6 +8,8 @@
 
 #import "TouchIDViewController.h"
 
+@import LocalAuthentication;
+
 @interface TouchIDViewController ()
 
 @end
@@ -21,7 +23,58 @@
 
 - (void)authenticateWithTouchID
 {
+    LAContext *laContext = [LAContext new];
     
+    NSError *error = nil;
+    
+    if ([laContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+        [laContext evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:@"Lets test your finger print" reply:^(BOOL success, NSError *error) {
+            
+            NSString *alertTitle = nil;
+            NSString *alertMessage = nil;
+            
+            if (success) {
+                alertTitle = @"Success";
+                alertMessage = @"It worked";
+            }
+            else if (error) {
+                switch (error.code) {
+                    case LAErrorUserFallback:
+                        alertTitle = @"Cancelled";
+                        alertMessage = @"Fallback to another auth mechanism";
+                        break;
+                    case LAErrorUserCancel:
+                        // User tapped cancelled
+                        break;
+                    default:
+                        alertTitle = @"Error";
+                        alertMessage = @"Error reading finger print";
+                        break;
+                }
+            }
+            else {
+                alertTitle = @"Failed";
+                alertMessage = @"Finger print doesn't match";
+            }
+
+            if (alertTitle || alertMessage) {
+                [self showTitle:alertTitle andMessage:alertMessage];
+            }
+        }];
+    }
+    else {
+        [self showTitle:@"Error" andMessage:@"Touch ID is not setup"];
+    }
+    
+}
+
+- (void)showTitle:(NSString *)title andMessage:(NSString *)message
+{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [alertController dismissViewControllerAnimated:YES completion:nil];
+    }]];
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 @end
